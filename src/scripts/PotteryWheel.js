172a6afcf0a65effb396e790de8d630b120d6b4b
nickglovdev <nickglovdev@gmail.com>
2020-04-21 08:38:29 -0500

    Got stuck on the for loop on id. Should have just passed it on, and hardcoded the ids

--- src/scripts/PotteryWheel.js
+++ src/scripts/PotteryWheel.js
@@ -1,13 +1,14 @@
 const pottery = {"id": 1}
 
-export const makePottery = (shape, weight, height) => {
-    for(let i=0; i < 5; i++){
-        pottery.id = i++
-    }
+export const makePottery = (shape, weight, height, id) => {
+    for(let i=1; i <= 5; i++){
+      console.log(i)
+        pottery.id + i
+    
         pottery.height = height
         pottery.weight = weight
         pottery.shape = shape
-    
+    }
     return pottery
 }
 
